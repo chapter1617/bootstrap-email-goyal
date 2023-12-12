f1b542e092e1fc12391d9e5c30162ca4839f9778
chapter1617 <goyal.mansi16@gmail.com>
2023-12-11 21:06:34 -0800

    adding js files to js folder

---
+++ assets/js/custom.js
@@ -0,0 +1,18 @@
+// $(document).ready(function () {
+//     $('.toggleClick').click(function () {
+//         $('.toggleBox').toggleClass('toggle');
+//     })
+// });
+
+
+$(document).ready(function () {
+    $(".toggleClick").click(function () {
+        $(".toggleBox").toggleClass("toggle");
+    });
+
+
+    $(".togglelist").click(function () {
+        $(".togglelistModal").toggleClass("toggle");
+    });
+
+});
